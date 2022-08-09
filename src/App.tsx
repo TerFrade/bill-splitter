e5dc8f68262657b619bd0502a8806ae01217e91d
TerFrade <terence.frade@wundermanthompson.com>
2022-08-09 21:41:39 +0200

    minor changes and experimenting folder architecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from "react"
+import styled from "styled-components"
 
-import "./App.css"
+import { Card } from "./elements"
 
 function App() {
-  return <div>Words</div>
+  return (
+    <Container>
+      <Card>
+        <div>hello</div>
+      </Card>
+    </Container>
+  )
 }
+
+const Container = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+`
 
 export default App
